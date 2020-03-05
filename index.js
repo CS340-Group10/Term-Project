39c7820db14ea6da83aaf881bb24661ebd51eb83
Gerson <lindorjr.0317@gmail.com>
2020-03-05 01:32:06 -0800

    initial commit: handlebars are already set up

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const exphbs = require('express-handlebars');
+
+const PORT = process.env.PORT || 5000;
+
+// Set up handlebars
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+
+// set up routes
+// route for home page
+app.get('/', function(req, res) {
+	res.render('home');
+});
+
+// route for reports page
+app.get('/reports', function(req, res) {
+	res.render('reports');
+});
+
+
+// route for charts page
+app.get('/charts', function(req, res) {
+	res.render('charts');
+});
+
+// route for admin page
+app.get('/admin', function(req, res) {
+	res.render('admin');
+});
+
+
+// This are routes for static html pages
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// listen on port
+app.listen(PORT, () => console.log("Server Listening on port " + PORT));
