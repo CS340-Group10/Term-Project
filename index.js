3f0223657e76b3dcba289350c03bdc499010b604
Gerson <lindorjr.0317@gmail.com>
2020-03-06 22:48:54 -0800

    Bug: SQL select query not working

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const exphbs = require('express-handlebars');
+const mysql = require('./dbconnect.js');
+// setup body parser
+const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 5000;
 
 // Set up handlebars
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
+
+// create application/x-www-form-urlencoded parser
+const urlencodedParser = bodyParser.urlencoded({ extended: false});
 
 
 // set up routes
@@ -18,11 +24,50 @@ app.get('/', function(req, res) {
 	});
 });
 
+
 // route for reports page
 app.get('/reports', function(req, res) {
 	res.render('reports', {
 		title: "reports page"
 	});
+});
+
+/*
+// select query for reports page
+app.get('/reports', function(req, res) {
+	// sql query
+//	var dml = 'SELECT team_name, revenue, city, state, sport FROM teams JOIN sports ' +
+//			  'ON teams.team_id = sports.sport_id';
+	mysql.pool.query('SELECT * FROM sports', function(error, results, fields) {
+		if (error) {
+			console.log("Error Bad query"); 
+			throw error;
+		}
+		console.log("Result of query: ");
+		console.log(results);
+		res.render('reports', {
+			results: results
+		});
+	}); 
+});
+*/
+// select query for reports page
+app.get('/reports', function(req, res) {
+	// sql query
+/*	var dml = 'SELECT team_name, revenue, city, state, sport FROM teams JOIN sports ' +
+			  'ON teams.team_id = sports.sport_id';
+*/
+	var context = {};
+	mysql.pool.query('SELECT * FROM sports', function(error, results, fields) {
+		if (error) {
+			console.log("Error Bad query"); 
+			throw error;
+		}
+		console.log("Result of query: ");
+		console.log(results);
+		context.myResult = JSON.stringify(results)
+		res.render('reports', context);
+	}); 
 });
 
 
@@ -32,6 +77,7 @@ app.get('/charts', function(req, res) {
 		title: "charts page"
 	});
 });
+
 
 // route for admin page
 app.get('/admin', function(req, res) {
