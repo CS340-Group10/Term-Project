0d0269d8d23d29ce09c140df2b7789452fc6fa7f
ChasenYamashita <32147061+ChasenYamashita@users.noreply.github.com>
2020-03-07 16:04:42 -0800

    Display from Teams on Admin

--- admin.js
+++ admin.js
@@ -13,14 +13,31 @@ module.exports = function(){
             complete();
         });
     }
+	
+	function getTeams(res, mysql, context, complete){
+		var myQuery = 'SELECT team_name, active_salary_cap, city, state, sport_name FROM teams ' +
+					'JOIN sports ON teams.sport = sports.sport_id';
+        mysql.pool.query(myQuery, function(error, results, fields){
+		if (error) {
+			console.log("Error Bad query"); 
+			throw error;
+		}
+            context.teams = results;
+            complete();
+        });
+    }
 
     router.get('/', function(req, res){
         var callbackCount = 0;
 		var context = {};
         var mysql = req.app.get('mysql');
         getSports(res, mysql, context, complete);
+        getTeams(res, mysql, context, complete);
         function complete(){
+			callbackCount++;
+            if(callbackCount >= 2){
                 res.render('admin', context);
+			}
         }
     });
 
